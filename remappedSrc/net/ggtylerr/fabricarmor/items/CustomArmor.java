e42358a5d83adba8546f813ff891f385fba03229
ggtylerr <ggtylerr_contact@protonmail.com>
2020-07-28 20:25:01 -0700

    Update to 1.16.1

--- remappedSrc/net/ggtylerr/fabricarmor/items/CustomArmor.java
+++ remappedSrc/net/ggtylerr/fabricarmor/items/CustomArmor.java
@@ -2,19 +2,22 @@ package net.ggtylerr.fabricarmor.items;
 
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.ggtylerr.fabricarmor.FabricArmor;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.item.ArmorMaterial;
+import net.minecraft.item.ItemConvertible;
 import net.minecraft.item.Items;
 import net.minecraft.recipe.Ingredient;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.Lazy;
+import net.minecraft.recipe.ArmorDyeRecipe;
 
 import java.util.function.Supplier;
 
 public enum CustomArmor implements ArmorMaterial {
-    FABRIC("leather", 5, new int[]{1, 4, 5, 2}, 15, SoundEvents.ITEM_ARMOR_EQUIP_LEATHER, 0.0F, 0.0F, () -> {
-        return Ingredient.ofItems(Items.LEATHER);
+    FABRIC("leather", 5, new int[]{1, 4, 5, 2}, 15, SoundEvents.ITEM_ARMOR_EQUIP_LEATHER, 0.0F, () -> {
+        return Ingredient.ofItems(new ItemConvertible[]{Items.LEATHER});
     });
 
     private static final int[] BASE_DURABILITY = new int[]{13, 15, 16, 11};
@@ -24,17 +27,15 @@ public enum CustomArmor implements ArmorMaterial {
     private final int enchantability;
     private final SoundEvent equipSound;
     private final float toughness;
-    private final float knockbackResistance;
     private final Lazy<Ingredient> repairIngredientSupplier;
 
-    private CustomArmor(String string_1, int int_1, int[] ints_1, int int_2, SoundEvent soundEvent_1, float float_1, float float_2, Supplier<Ingredient> supplier_1) {
+    private CustomArmor(String string_1, int int_1, int[] ints_1, int int_2, SoundEvent soundEvent_1, float float_1, Supplier<Ingredient> supplier_1) {
         this.name = string_1;
         this.durabilityMultiplier = int_1;
         this.protectionAmounts = ints_1;
         this.enchantability = int_2;
         this.equipSound = soundEvent_1;
         this.toughness = float_1;
-        this.knockbackResistance = float_2;
         this.repairIngredientSupplier = new Lazy(supplier_1);
     }
 
@@ -65,10 +66,5 @@ public enum CustomArmor implements ArmorMaterial {
 
     public float getToughness() {
         return this.toughness;
-    }
-
-    @Override
-    public float getKnockbackResistance() {
-        return this.knockbackResistance;
     }
 }
---
+++ remappedSrc/net/ggtylerr/fabricarmor/items/FabricArmorItem.java
@@ -0,0 +1,11 @@
+package net.ggtylerr.fabricarmor.items;
+
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.item.ArmorItem;
+import net.minecraft.item.ArmorMaterial;
+
+public class FabricArmorItem extends ArmorItem {
+    public FabricArmorItem(ArmorMaterial armorMaterial_1, EquipmentSlot equipmentSlot_1, Settings item$Settings_1) {
+        super(armorMaterial_1, equipmentSlot_1, item$Settings_1);
+    }
+}
